81b9a502b6f24c2228096d81c52cb4b701e587fd
luciano.silva <luciano.silva@ifpb.edu.br>
2019-05-09 22:29:16 -0300

    criacao de elentos html de uma tabela

--- paginaAjax.js
+++ paginaAjax.js
@@ -11,23 +11,30 @@ function buscarDisciplinas(){
     
     if(request.status==200){
         // recebemos uma resposta positiva
-        resultado = request.responseText;
+        resultado = JSON.parse(request.responseText);
+        console.log(resultado);
     }else{
         //alguma coisa deu errado
         alert("Error"+request.status+":"+request.statusText);
     }
-    resultado = request.responseText;
+    
    var content = document.getElementById("content");
-                content.innerText= resultado;
+   var table =   document.createElement("table");
+   content.appendChild(table);
+   var tr = document.createElement("tr");
+   table.appendChild(tr);
+   var td = document.createElement("td");
+   tr.appendChild(td);
+   td.innerText=(resultado[0]);
 }
 
 
 document.onreadystatechange = function(){
-    //if(document.readystate == "complete"){
+    if(document.readyState === "complete"){
         
-        alert("Alguma coisa pode acontecer");
+        //alert("Alguma coisa pode acontecer");
         buscarDisciplinas();
-    //}
+    }
 }
 
 
